addb3c5ced7eb2080de1cf19d33a9117d8707971
Gaurav Trivedi <trivedigaurav@gmail.com>
2019-11-17 17:03:09 -0500

    Add sample wordtree

--- main.js
+++ main.js
@@ -9,11 +9,11 @@ let mainWindow
 function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1200,
+    height: 800,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
-    }
+    },
   })
 
   // and load the index.html of the app.
---
+++ wordtree-interface.js
@@ -0,0 +1,15 @@
+function getSentenceStats() {
+  	updateSentenceStats(data.matches);
+}
+
+function updateSentenceStats(matches){
+	total = data.total;
+  	percentange = 100*matches/total;
+  	console.log("Documents included: "+ 
+                            matches+"/"+total+
+                            " ( "+percentange.toFixed(2)+"% ) ");
+}
+
+function updateFeedback(selected, root){
+	// JavaFXApp.updateFeedback(selected, root);
+}
